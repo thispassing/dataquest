9e1bf9d07cf5a1a1d15eb441d938e01ec9c23fd7
thispassing <thispassing@users.noreply.github.com>
2019-03-28 17:31:00 -0400

    improved function by adding parameters

--- apple.py
+++ apple.py
@@ -3,21 +3,16 @@ from csv import reader
 read_file = reader(opened_file)
 apps_data = list(read_file)
 
-def freq_table(col_num):
-    column = []    
+def freq_table(index, data_set):
+    frequency_table = {}
+    for row in data_set[1:]:
+        value = row[index]
     
-    for row in apps_data[1:]:
-        value = row[col_num]
-        column.append(value)
-        
-    ft_dict = {}
-    
-    for x in column:
-        if x in ft_dict:
-            ft_dict[x] += 1
+        if value in frequency_table:
+            frequency_table[value] += 1
         else:
-            ft_dict[x] = 1
-    
-    return ft_dict
+            frequency_table[value] = 1
+            
+    return frequency_table
 
-ratings_ft = freq_table(7)
+ratings_ft = freq_table(7, apps_data)
